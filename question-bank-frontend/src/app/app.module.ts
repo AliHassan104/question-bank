bdee64f03dce9f84c33ed8573c3a45c7ed1b9061
Ali Hassan <alihassan.uok@gmail.com>
2024-09-22 23:34:13 +0500

    first

---
+++ question-bank-frontend/src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ToastrModule } from "ngx-toastr";
+
+import { SidebarModule } from './sidebar/sidebar.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { NavbarModule} from './shared/navbar/navbar.module';
+import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
+
+import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ClassesComponent } from './pages/classes/classes.component';
+import { AddClassComponent } from './pages/classes/add-class/add-class.component';
+import { ViewClassComponent } from './pages/classes/view-class/view-class.component';
+import { SubjectComponent } from './pages/subject/subject.component';
+import { ViewSubjectComponent } from './pages/subject/view-subject/view-subject.component';
+import { AddSubjectComponent } from './pages/subject/add-subject/add-subject.component';
+import { ChapterComponent } from './pages/chapter/chapter.component';
+import { AddChapterComponent } from './pages/chapter/add-chapter/add-chapter.component';
+import { ViewChapterComponent } from './pages/chapter/view-chapter/view-chapter.component';
+import { QuestionComponent } from './pages/question/question.component';
+import { ViewQuestionComponent } from './pages/question/view-question/view-question.component';
+import { AddQuestionComponent } from './pages/question/add-question/add-question.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    ClassesComponent,
+    AddClassComponent,
+    ViewClassComponent,
+    SubjectComponent,
+    ViewSubjectComponent,
+    AddSubjectComponent,
+    ChapterComponent,
+    AddChapterComponent,
+    ViewChapterComponent,
+    QuestionComponent,
+    ViewQuestionComponent,
+    AddQuestionComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    RouterModule.forRoot(AppRoutes,{
+      useHash: true
+    }),
+    SidebarModule,
+    NavbarModule,
+    ToastrModule.forRoot(),
+    FooterModule,
+    FixedPluginModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
